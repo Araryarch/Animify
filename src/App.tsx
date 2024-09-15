1ffea83b2e53c13efe6a64c7ba1c70648a599fc3
Araryarch <araryaarch@gmail.com>
2024-09-15 21:23:29 +0700

    build(deps): update package-lock.json, package.json, App.tsx and Hero.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
 import Hero from './Components/Pages/Hero'
+import AnimatedCursor from 'react-animated-cursor'
 
 const App = () => {
-  return <Hero />
+  return (
+    <>
+      <AnimatedCursor />
+      <Hero />
+    </>
+  )
 }
 
 export default App
--- src/Components/Pages/Hero.tsx
+++ src/Components/Pages/Hero.tsx
@@ -6,11 +6,9 @@ const Hero = () => {
     <Background>
       <div className="h-[50rem] w-full bg-dot-white/[0.2] relative flex items-center justify-center">
         <div className="absolute pointer-events-none inset-0 flex items-center justify-center [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
-        <p className="relative z-20 py-8 text-4xl font-bold text-transparent sm:text-7xl bg-clip-text bg-gradient-to-b from-neutral-200 to-neutral-500">
-          <div className="h-[25rem] flex items-center justify-center">
-            <TextHover text="ANIMIFY" duration={100} />
-          </div>
-        </p>
+        <div className="h-[30rem] flex items-center justify-center p-5">
+          <TextHover text="ANIMIFY" duration={100} />
+        </div>
       </div>
     </Background>
   )
