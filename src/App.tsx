a2fd07c286f486b266cf1fc350e69f6ae03f942d
Araryarch <araryaarch@gmail.com>
2024-09-15 21:30:42 +0700

    update App.tsx and background.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,19 @@ import AnimatedCursor from 'react-animated-cursor'
 const App = () => {
   return (
     <>
-      <AnimatedCursor />
+      <AnimatedCursor
+        innerSize={8}
+        outerSize={35}
+        innerScale={1}
+        outerScale={1.7}
+        outerAlpha={0}
+        outerStyle={{
+          border: '3px solid white',
+        }}
+        innerStyle={{
+          backgroundColor: 'white',
+        }}
+      />
       <Hero />
     </>
   )
