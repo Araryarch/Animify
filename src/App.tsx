d98a5878b61e70a46ea57bb240e53bf3ae91801a
Araryarch <araryaarch@gmail.com>
2024-09-16 12:47:19 +0700

    build(deps): update package-lock.json, package.json and App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,27 @@
 import Hero from './Components/Pages/Hero'
 import AnimatedCursor from 'react-animated-cursor'
 import Navbar from './Components/ui/Navbar'
+import { isMobile } from 'react-device-detect'
 
 const App = () => {
   return (
     <>
-      <AnimatedCursor
-        innerSize={8}
-        outerSize={50}
-        innerScale={1}
-        outerScale={1.7}
-        outerAlpha={0}
-        outerStyle={{
-          border: '3px solid #c026d3',
-        }}
-        innerStyle={{
-          mixBlendMode: 'difference',
-          backgroundColor: '#fffffff',
-        }}
-      />
+      {!isMobile && (
+        <AnimatedCursor
+          innerSize={8}
+          outerSize={50}
+          innerScale={1}
+          outerScale={1.7}
+          outerAlpha={0}
+          outerStyle={{
+            border: '3px solid #c026d3',
+          }}
+          innerStyle={{
+            mixBlendMode: 'difference',
+            backgroundColor: '#fffffff',
+          }}
+        />
+      )}
       <Hero />
       <Navbar />
     </>
