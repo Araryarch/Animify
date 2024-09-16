df52a698beecdf17e0ebaa15beda2e745daaef5a
Araryarch <araryaarch@gmail.com>
2024-09-16 13:34:10 +0700

    delete 2 files and update 2 files

--- src/Components/ui/background.tsx
+++ src/Components/ui/background.tsx
@@ -75,7 +75,12 @@ export const Background = ({
           className
         )}
       >
-        <div className="absolute top-0 bottom-0 left-0 right-0 bg-[url('./Image/background.jpg')] brightness-[20%]"></div>
+        <div
+          style={{
+            backgroundImage: 'url(./assets/background.jpg)',
+          }}
+          className="absolute top-0 bottom-0 left-0 right-0 brightness-[20%] bg-cover bg-center"
+        ></div>
 
         {beams.map((beam) => (
           <CollisionMechanism
@@ -89,7 +94,7 @@ export const Background = ({
         {children}
         <div
           ref={containerRef}
-          className="absolute bottom-0 bg=[#1e1e2e] w-full inset-x-0 pointer-events-none"
+          className="absolute bottom-0 bg-[#1e1e2e] w-full inset-x-0 pointer-events-none"
           style={{
             boxShadow:
               '0 0 24px rgba(34, 42, 53, 0.06), 0 1px 1px rgba(0, 0, 0, 0.05), 0 0 0 1px rgba(34, 42, 53, 0.04), 0 0 4px rgba(34, 42, 53, 0.08), 0 16px 68px rgba(47, 48, 55, 0.05), 0 1px 0 rgba(255, 255, 255, 0.1) inset',
