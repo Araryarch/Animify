40aa472077b09fead602c6246a24027ab6d510cc
Araryarch <araryaarch@gmail.com>
2024-09-17 02:05:45 +0700

    update Topbar.tsx

--- src/Components/ui/Topbar.tsx
+++ src/Components/ui/Topbar.tsx
@@ -8,14 +8,14 @@ const Topbar = ({ title, pages }: topbarProps) => {
     <>
       <div className="absolute top-0 left-0 right-0 flex items-center justify-center p-16 text-white">
         <div className="flex-1 border-[1px]"></div>
-        <div className="flex-1 border-[1px]"></div>
+        <div className="flex-1 border-[1px] xl:flex hidden"></div>
         <div className="flex-1"></div>
         <div className="flex flex-col items-center justify-center flex-initial text-5xl">
           <h1 className="logo">{title}</h1>
           <p className="text-xs logo">section</p>
         </div>
         <div className="flex-1"></div>
-        <div className="flex-1">PAGE - {pages} / 06</div>
+        <div className="flex-1 hidden xl:flex">PAGE - {pages} / 06</div>
         <div className="flex-1 border-[1px]"></div>
       </div>
       <div className="h-44"></div>
