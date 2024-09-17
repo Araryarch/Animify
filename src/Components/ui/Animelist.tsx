bcf05d1f9a91ac820f186c5eabb49774259c5085
Araryarch <araryaarch@gmail.com>
2024-09-17 14:36:22 +0700

    update Animelist.tsx and Mangalist.tsx

--- src/Components/ui/Animelist.tsx
+++ src/Components/ui/Animelist.tsx
@@ -26,7 +26,26 @@ const AnimeList: React.FC<Propstype> = ({ classname }) => {
     getAnimeList()
   }, [])
 
-  if (loading) return <p>Loading...</p>
+  if (loading)
+    return (
+      <div
+        className={`flex flex-col gap-2 text-white ${classname} px-24 transition-all duration-300 ease-in-out`}
+      >
+        <h1 className="text-3xl font-bold">TOP ANIME</h1>
+        <ul className="grid grid-cols-2 gap-5 sm:grid-cols-3 xl:grid-cols-5 md:grid-cols-4">
+          {Array.from({ length: 10 }, (_, index) => (
+            <li
+              key={index}
+              className="flex flex-col items-center justify-center p-2 bg-gray-700 rounded shadow-sm animate-pulse"
+            >
+              <div className="w-32 h-40 bg-gray-600 rounded"></div>
+              <div className="w-24 h-6 mt-2 bg-gray-600 rounded"></div>
+            </li>
+          ))}
+        </ul>
+      </div>
+    )
+
   if (error) return <p>{error}</p>
 
   return (
--- src/Components/ui/Mangalist.tsx
+++ src/Components/ui/Mangalist.tsx
@@ -6,7 +6,7 @@ interface Propstype {
 }
 
 const MangaList: React.FC<Propstype> = ({ classname }) => {
-  const [MangaList, setMangaList] = useState<Manga[]>([])
+  const [mangaList, setMangaList] = useState<Manga[]>([])
   const [loading, setLoading] = useState(true)
   const [error, setError] = useState<string | null>(null)
 
@@ -26,7 +26,26 @@ const MangaList: React.FC<Propstype> = ({ classname }) => {
     getMangaList()
   }, [])
 
-  if (loading) return <p>Loading...</p>
+  if (loading)
+    return (
+      <div
+        className={`flex flex-col gap-2 text-white ${classname} px-24 transition-all duration-300 ease-in-out`}
+      >
+        <h1 className="text-3xl font-bold">TOP MANGA</h1>
+        <ul className="grid grid-cols-2 gap-5 sm:grid-cols-3 xl:grid-cols-5 md:grid-cols-4">
+          {Array.from({ length: 10 }, (_, index) => (
+            <li
+              key={index}
+              className="flex flex-col items-center justify-center p-2 bg-gray-700 rounded shadow-sm animate-pulse"
+            >
+              <div className="w-32 h-40 bg-gray-600 rounded"></div>
+              <div className="w-24 h-6 mt-2 bg-gray-600 rounded"></div>
+            </li>
+          ))}
+        </ul>
+      </div>
+    )
+
   if (error) return <p>{error}</p>
 
   return (
@@ -35,13 +54,13 @@ const MangaList: React.FC<Propstype> = ({ classname }) => {
     >
       <h1 className="text-3xl font-bold">TOP MANGA</h1>
       <ul className="grid grid-cols-2 gap-5 sm:grid-cols-3 xl:grid-cols-5 md:grid-cols-4">
-        {MangaList.map((Manga) => (
+        {mangaList.map((manga) => (
           <li
-            key={Manga.mal_id}
+            key={manga.mal_id}
             className="flex flex-col items-center justify-center p-2 shadow-sm shadow-fuchsia-950"
           >
-            <img src={Manga.images.jpg.image_url} alt={Manga.title} />
-            <h1>{Manga.title}</h1>
+            <img src={manga.images.jpg.image_url} alt={manga.title} />
+            <h1>{manga.title}</h1>
           </li>
         ))}
       </ul>
