79c195b4612cd8e2f1cbc70e20e934578b41500f
Araryarch <araryaarch@gmail.com>
2024-09-17 14:53:14 +0700

    update Animelist.tsx and Mangalist.tsx

--- src/Components/ui/Animelist.tsx
+++ src/Components/ui/Animelist.tsx
@@ -53,7 +53,7 @@ const AnimeList: React.FC<Propstype> = ({ classname }) => {
       className={`flex flex-col gap-2 text-white ${classname} px-24 transition-all duration-300 ease-in-out`}
     >
       <h1 className="text-3xl font-bold">TOP ANIME</h1>
-      <ul className="grid grid-cols-2 gap-5 sm:grid-cols-3 xl:grid-cols-5 md:grid-cols-4">
+      <ul className="grid grid-cols-1 gap-5 sm:grid-cols-2 xl:grid-cols-5 md:grid-cols-3">
         {animeList.map((anime) => (
           <li
             key={anime.mal_id}
