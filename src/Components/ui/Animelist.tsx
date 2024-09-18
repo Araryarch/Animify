5dba79bbcb3f85a2c2b9209ecd77b1150bc87ae6
Araryarch <araryaarch@gmail.com>
2024-09-18 14:33:52 +0700

    update Animelist.tsx and Mangalist.tsx

--- src/Components/ui/Animelist.tsx
+++ src/Components/ui/Animelist.tsx
@@ -58,21 +58,28 @@ const AnimeList: React.FC<Propstype> = ({ classname }) => {
     prevArrow: <PrevArrow />,
     responsive: [
       {
-        breakpoint: 1300,
+        breakpoint: 1600,
         settings: {
-          slidesToShow: 4,
-          slidesToScroll: 4,
+          slidesToShow: 5,
+          slidesToScroll: 5,
         },
       },
       {
-        breakpoint: 1024,
+        breakpoint: 1300,
         settings: {
           slidesToShow: 3,
           slidesToScroll: 3,
         },
       },
       {
-        breakpoint: 600,
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+        },
+      },
+      {
+        breakpoint: 768,
         settings: {
           slidesToShow: 2,
           slidesToScroll: 2,
@@ -87,6 +94,7 @@ const AnimeList: React.FC<Propstype> = ({ classname }) => {
       },
     ],
   }
+
   if (loading)
     return (
       <div
--- src/Components/ui/Mangalist.tsx
+++ src/Components/ui/Mangalist.tsx
@@ -58,21 +58,28 @@ const MangaList: React.FC<Propstype> = ({ classname }) => {
     prevArrow: <PrevArrow />,
     responsive: [
       {
-        breakpoint: 1300,
+        breakpoint: 1600,
         settings: {
-          slidesToShow: 4,
-          slidesToScroll: 4,
+          slidesToShow: 5,
+          slidesToScroll: 5,
         },
       },
       {
-        breakpoint: 1024,
+        breakpoint: 1300,
         settings: {
           slidesToShow: 3,
           slidesToScroll: 3,
         },
       },
       {
-        breakpoint: 600,
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+        },
+      },
+      {
+        breakpoint: 768,
         settings: {
           slidesToShow: 2,
           slidesToScroll: 2,
