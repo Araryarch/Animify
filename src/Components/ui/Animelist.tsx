38022bef8953e6079065fa729afad2e719b3a9c1
Araryarch <araryaarch@gmail.com>
2024-09-18 11:21:05 +0700

    build(deps): update package-lock.json, Animelist.tsx and main.tsx

--- src/Components/ui/Animelist.tsx
+++ src/Components/ui/Animelist.tsx
@@ -1,10 +1,30 @@
 import React, { useEffect, useState } from 'react'
+import Slider from 'react-slick'
 import { fetchAnimeList, Anime } from '../../service/apiService'
 import { Link } from 'react-router-dom'
+import { FaChevronLeft, FaChevronRight } from 'react-icons/fa'
 
 interface Propstype {
   classname: string
 }
+
+const NextArrow = ({ onClick }: { onClick?: () => void }) => (
+  <div
+    className="absolute z-50 text-4xl text-white transform -translate-y-1/2 cursor-pointer -right-8 top-1/2"
+    onClick={onClick}
+  >
+    <FaChevronRight />
+  </div>
+)
+
+const PrevArrow = ({ onClick }: { onClick?: () => void }) => (
+  <div
+    className="absolute z-50 text-4xl text-white transform -translate-y-1/2 cursor-pointer -left-8 top-1/2"
+    onClick={onClick}
+  >
+    <FaChevronLeft />
+  </div>
+)
 
 const AnimeList: React.FC<Propstype> = ({ classname }) => {
   const [animeList, setAnimeList] = useState<Anime[]>([])
@@ -27,6 +47,36 @@ const AnimeList: React.FC<Propstype> = ({ classname }) => {
     getAnimeList()
   }, [])
 
+  const settings = {
+    dots: false,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 5,
+    slidesToScroll: 3,
+    nextArrow: <NextArrow />,
+    prevArrow: <PrevArrow />,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+        },
+      },
+    ],
+  }
+
   if (loading)
     return (
       <div
@@ -34,10 +84,10 @@ const AnimeList: React.FC<Propstype> = ({ classname }) => {
       >
         <h1 className="text-3xl font-bold">TOP ANIME</h1>
         <ul className="grid grid-cols-1 gap-5 sm:grid-cols-2 xl:grid-cols-5 md:grid-cols-3">
-          {Array.from({ length: 10 }, (_, index) => (
+          {Array.from({ length: 5 }, (_, index) => (
             <li
               key={index}
-              className="flex flex-col items-center justify-center p-2 bg-gray-700 rounded shadow-sm animate-pulse"
+              className="flex flex-col items-center justify-center p-2 bg-gray-700 rounded shadow-sm animate-pulse aspect-[9/16]"
             >
               <div className="w-32 h-40 bg-gray-600 rounded"></div>
               <div className="w-24 h-6 mt-2 bg-gray-600 rounded"></div>
@@ -54,28 +104,25 @@ const AnimeList: React.FC<Propstype> = ({ classname }) => {
       className={`flex flex-col gap-2 text-white ${classname} px-24 transition-all duration-300 ease-in-out py-2`}
     >
       <h1 className="text-3xl font-bold">TOP ANIME</h1>
-      <ul className="grid grid-cols-1 gap-5 sm:grid-cols-2 xl:grid-cols-5 md:grid-cols-3">
+      <Slider {...settings}>
         {animeList.map((anime) => (
-          <li
+          <Link
+            to={`/anime/${anime.mal_id}`}
             key={anime.mal_id}
-            className="flex flex-col items-center justify-center p-2 shadow-sm shadow-white bg-gradient-to-t from-black from-5% to-fuchsia-950"
+            className="card"
           >
-            <Link
-              to={`/anime/${anime.mal_id}`}
-              className="relative w-full h-full overflow-hidden"
+            <div
+              className="image-card aspect-[9/16] bg-cover mx-4 rounded-sm relative"
+              style={{ backgroundImage: `url(${anime.images.jpg.image_url})` }}
             >
-              <img
-                src={anime.images.jpg.image_url}
-                alt={anime.title}
-                className="object-cover transition-transform duration-300 ease-in-out transform hover:scale-110"
-              />
-            </Link>
-            <div className="flex items-center justify-center p-2">
-              <h1 className="font-bold text-center">{anime.title}</h1>
+              <div className="absolute top-0 bottom-0 left-0 right-0"></div>
             </div>
-          </li>
+            <div className="py-2 font-bold text-center hover:text-fuchsia-400">
+              {anime.title}
+            </div>
+          </Link>
         ))}
-      </ul>
+      </Slider>
     </div>
   )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,9 @@ import { BrowserRouter } from 'react-router-dom'
 import App from './App.tsx'
 import './index.css'
 import './App.css'
+// In your main CSS file or import in your component
+import 'slick-carousel/slick/slick.css'
+import 'slick-carousel/slick/slick-theme.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
