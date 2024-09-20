93824bdd232e7e35473596cd7a8feb2d78f78b85
Araryarch <araryaarch@gmail.com>
2024-09-20 12:34:11 +0700

    build(deps): update package-lock.json, package.json and main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -4,13 +4,14 @@ import { BrowserRouter } from 'react-router-dom'
 import App from './App.tsx'
 import './index.css'
 import './App.css'
-// In your main CSS file or import in your component
 import 'slick-carousel/slick/slick.css'
 import 'slick-carousel/slick/slick-theme.css'
+import { Analytics } from '@vercel/analytics/react'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
+      <Analytics />
       <App />
     </BrowserRouter>
   </StrictMode>
